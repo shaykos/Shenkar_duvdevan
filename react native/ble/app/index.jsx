f9105709d27200c22c8cbe52fbe98c6f7cd30da3
Shay Abraham <shaykos@gmail.com>
2025-05-22 14:57:40 +0300

    added ble - part 1

---
+++ react native/ble/app/index.jsx
@@ -0,0 +1,77 @@
+import { useEffect } from "react";
+import { PermissionsAndroid, Platform, Text, View } from "react-native";
+
+export default function Index() {
+  const [blePermissions, setBlePermissions] = useState(null);
+
+  async function requestBLEPermission() {
+    const bluetoothScanPermission = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+      {
+        title: "Location Permission",
+        message: "Bluetooth Low Energy requires Location",
+        buttonPositive: "OK",
+      }
+    );
+    const bluetoothConnectPermission = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+      {
+        title: "Location Permission",
+        message: "Bluetooth Low Energy requires Location",
+        buttonPositive: "OK",
+      }
+    );
+    const fineLocationPermission = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: "Location Permission",
+        message: "Bluetooth Low Energy requires Location",
+        buttonPositive: "OK",
+      }
+    );
+
+    return (
+      bluetoothScanPermission === "granted" &&
+      bluetoothConnectPermission === "granted" &&
+      fineLocationPermission === "granted"
+    );
+
+  }
+
+  async function checkPermissions() {
+    if (Platform.OS === "android") {
+      if ((ExpoDevice.platformApiLevel ?? -1) < 31) {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          {
+            title: "Location Permission",
+            message: "Bluetooth Low Energy requires Location",
+            buttonPositive: "OK",
+          }
+        );
+        setBlePermissions(granted === PermissionsAndroid.RESULTS.GRANTED);
+      } else {
+        let permissions = await requestBLEPermission();
+        setBlePermissions(permissions);
+      }
+    } else {
+      setBlePermissions(true);
+    }
+  }
+
+  useEffect(async () => {
+    checkPermissions();
+  }, []);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
